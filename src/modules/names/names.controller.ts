05c243dd64edb2333865163630fc200eba11971b
Sergi Casanova Enguix <sergicasanova6798@gmail.com>
2024-03-06 16:31:37 +0100

    "llamadas Post/Get/Put creadas"

--- src/modules/names/names.controller.ts
+++ src/modules/names/names.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query, Param, Put } from '@nestjs/common';
 import { NamesService } from './names.service';
 
 
@@ -9,5 +9,22 @@ export class NamesController {
     constructor(private namesService: NamesService){
 
     }
+
+    @Post()//decorador
+    createName(@Body() data: { name: string}){
+        return this.namesService.createName(data.name);
+    }
+
+    @Get()
+    getNames(@Query('start') start: string){
+        console.log(start)
+        return this.namesService.getNames(start);
+    }
+
+    @Put('/:name/:newName')
+    updateName(@Param('name') name: string, @Param('newName') newName: string){
+        return this.namesService.updateName(name, newName);
+    }
+    
 
 }
--- src/modules/names/names.service.ts
+++ src/modules/names/names.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NamesService {}
+export class NamesService {
+
+    private _names: string[];
+
+    constructor(){
+        this._names = [];
+    }
+
+    createName(name: string){
+
+        const nameFound = this._names.find(n => n.toLowerCase().trim() === name.toLowerCase().trim());
+
+        if(!nameFound){
+            this._names.push(name);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    getNames(start?: string){
+        if(!start){
+            return this._names;
+        }else{
+            return this._names.filter(n => n.toLowerCase().trim().startsWith(start.toLowerCase().trim()));
+        }
+    }
+
+    updateName(name: string, newName: string){
+        const indexNameFound = this._names.findIndex(n => n.toLowerCase().trim() == name.toLowerCase().trim());
+        const indexNewNameFound = this._names.findIndex(n => n.toLowerCase().trim() == newName.toLowerCase().trim());
+
+        //-1 es decir que no lo encuentra
+        if(indexNameFound != -1 && indexNewNameFound == -1){
+            this._names[indexNameFound] = newName;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
